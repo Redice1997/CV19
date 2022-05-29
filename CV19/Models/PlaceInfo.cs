75b459b721dae4de6505a3203eaf99d474092194
Redice1997 <roshin-vladimir@mail.ru>
2022-05-30 00:33:29 +0300

    Добавлены модели

---
+++ CV19/Models/PlaceInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV19.Models
+{
+    internal class PlaceInfo
+    {
+        public string Name { get; set; }
+        public Point Location { get; set; }
+        public IEnumerable<ConfirmedCount> Counts { get; set; }
+
+    }      
+}
